f6199e23c5ba741c68561640cbb68e4d9d4004c3
Fernando Vieira <fernandovieira1@outlook.com>
2024-08-08 03:14:39 +0000

    commit rscloud 07082024 - pacotes ibge

---
+++ fontes_dados.R
@@ -0,0 +1,31 @@
+#### 0. CONFIGURAR O AMBIENTE ####
+# 0.1 Carregar pacotes necessários ####
+library(readxl)
+library(tidyverse)
+library(googledrive)
+library(kableExtra)
+library(ggcorrplot)
+library(hrbrthemes)
+
+library(sidrar)
+library(geobr)
+
+## NOTAS ####
+# Talvez usar dados espaciais depois: https://github.com/ipeaGIT/geobr
+
+## IBGE (MUNIC; SIDRA): demografia, saúde e educação  ####
+
+## SIDRAR ####
+# https://cran.r-project.org/web/packages/sidrar/vignettes/Introduction_to_sidrar.html
+# search_sidra(c("gini"))
+# https://sidra.ibge.gov.br/home/pimpfbr/brasil
+# Apenas dados econômicos
+
+## GEOBR ####
+# https://github.com/ipeaGIT/geobr
+
+
+## MUNIC ####
+#https://sidra.ibge.gov.br/pesquisa/munic/tabelas
+
+
